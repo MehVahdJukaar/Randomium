20904d5d39f0d4cd55e0c2d9a4baaff87a2989d6
MehVahdJukaar <matteogozzi1998@gmail.com>
2025-06-15 12:08:57 +0200

    jei display thing

--- common/src/main/java/net/mehvahdjukaar/randomium/integration/JEICompat.java
+++ common/src/main/java/net/mehvahdjukaar/randomium/integration/JEICompat.java
@@ -11,7 +11,6 @@ import mezz.jei.api.registration.IRecipeRegistration;
 import mezz.jei.api.registration.IVanillaCategoryExtensionRegistration;
 import net.mehvahdjukaar.moonlight.api.platform.PlatHelper;
 import net.mehvahdjukaar.randomium.Randomium;
-import net.mehvahdjukaar.randomium.common.RandomiumDuplicateRecipe;
 import net.mehvahdjukaar.randomium.common.items.AnyItem;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
@@ -34,26 +33,31 @@ public class JEICompat implements IModPlugin {
 
     @Override
     public void registerVanillaCategoryExtensions(IVanillaCategoryExtensionRegistration registration) {
-        registration.getCraftingCategory().addExtension(RandomiumDuplicateRecipe.class, new DuplicateRecipeExtension());
+        registration.getCraftingCategory().addExtension(CraftingRecipe.class, new DuplicateRecipeExtension());
     }
 
     @Override
     public void registerRecipes(IRecipeRegistration registry) {
         if (!PlatHelper.isModLoaded("roughly_enough_items")) {
-            registry.addRecipes(RecipeTypes.CRAFTING, List.of(AnyItem.createDuplicateRecipe()));
+            RecipeHolder<CraftingRecipe> duplicateRecipe = AnyItem.createDuplicateRecipe();
+            registry.addRecipes(RecipeTypes.CRAFTING, List.of(duplicateRecipe));
         }
     }
 
+    static final ResourceLocation TARGET = Randomium.res("duplicate");
 
     private record DuplicateRecipeExtension() implements ICraftingCategoryExtension<CraftingRecipe> {
 
         @Override
         public void drawInfo(RecipeHolder<CraftingRecipe> recipe, int recipeWidth, int recipeHeight, GuiGraphics guiGraphics, double mouseX, double mouseY) {
-            guiGraphics.drawString(Minecraft.getInstance().font, Component.translatable("randomium.jei.duplicate"), 60, 46, 5592405);
+            if (recipe.id().equals(TARGET)) {
+                guiGraphics.drawString(Minecraft.getInstance().font, Component.translatable("randomium.jei.duplicate"), 60, 46, 5592405);
+            }
         }
 
         @Override
-        public void setRecipe(RecipeHolder<CraftingRecipe> recipeHolder, IRecipeLayoutBuilder builder, ICraftingGridHelper craftingGridHelper, IFocusGroup focuses) {
+        public void setRecipe(RecipeHolder<CraftingRecipe> recipeHolder, IRecipeLayoutBuilder
+                builder, ICraftingGridHelper craftingGridHelper, IFocusGroup focuses) {
         }
     }
 }
